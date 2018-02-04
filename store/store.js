a0c13c0f1c87f6828b14ad298fc5f8a935829250
kkashuda <kkashuda@gmail.com>
2018-02-03 20:52:41 -0500

    create store

---
+++ store/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+
+const item = (state = {title: ""}, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
+const rootReducer = combineReducers(item);
+let store = createStore(rootReducer);
+
+export default item; 
